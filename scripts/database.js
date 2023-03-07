da22222a1e5c3f6b9366e0c0ac281cee0c402fa5
Michelle Totherow <tym4me13@gmail.com>
2023-03-07 14:25:42 -0600

    First upload for Aquarium project

---
+++ scripts/database.js
@@ -0,0 +1,154 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            Id: 1,
+            Name: Sharkie,
+            Species: B. melanopterus,
+            Length: 2, 
+            Origin: "Malay Peninsula",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 2,
+            Name: Boss,
+            Species: B. splendens,
+            Length: 3, 
+            Origin: "Malay Peninsula",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 3,
+            Name: Bubba,
+            Species: B. melanopterus,
+            Length: 4, 
+            Origin: "Rio Negro",
+            Water: Fresh,
+            Diet: Carnivorous
+        },
+        {
+            Id: 4,
+            Name: Princess,
+            Species: P. scalare,
+            Length: 5, 
+            Origin: "Amazon Basin",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 5,
+            Name: Sara,
+            Species: B. melanopterus,
+            Length: 2, 
+            Origin: "Malay Peninsula",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 6,
+            Name: Gloss,
+            Species: B. splendens,
+            Length: 3, 
+            Origin: "Malay Peninsula",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 7,
+            Name: Bob,
+            Species: B. melanopterus,
+            Length: 4, 
+            Origin: "Rio Negro",
+            Water: Fresh,
+            Diet: Carnivorous
+        },
+        {
+            Id: 8,
+            Name: Prince,
+            Species: P. scalare,
+            Length: 5, 
+            Origin: "Amazon Basin",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 9,
+            Name: Susan,
+            Species: B. melanopterus,
+            Length: 2, 
+            Origin: "Malay Peninsula",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 10,
+            Name: Moss,
+            Species: B. splendens,
+            Length: 3, 
+            Origin: "Malay Peninsula",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 11,
+            Name: Daisy,
+            Species: B. melanopterus,
+            Length: 4, 
+            Origin: "Rio Negro",
+            Water: Fresh,
+            Diet: Carnivorous
+        },
+        {
+            Id: 12,
+            Name: King,
+            Species: P. scalare,
+            Length: 5, 
+            Origin: "Amazon Basin",
+            Water: Fresh,
+            Diet: Omnivore
+        }, 
+        {
+            Id: 13,
+            Name: Sam,
+            Species: B. melanopterus,
+            Length: 2, 
+            Origin: "Malay Peninsula",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 14,
+            Name: Ross,
+            Species: B. splendens,
+            Length: 3, 
+            Origin: "Malay Peninsula",
+            Water: Fresh,
+            Diet: Omnivore
+        },
+        {
+            Id: 15,
+            Name: Billy,
+            Species: B. melanopterus,
+            Length: 4, 
+            Origin: "Rio Negro",
+            Water: Fresh,
+            Diet: Carnivorous
+        },
+        {
+            Id: 16,
+            Name: Queen,
+            Species: P. scalare,
+            Length: 5, 
+            Origin: "Amazon Basin",
+            Water: Fresh,
+            Diet: Omnivore
+        }
+    ]
+}
