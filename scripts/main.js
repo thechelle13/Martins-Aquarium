a53626f55f9cd0aaa3bbe32196fdf4a50c89c4e8
Michelle Totherow <tym4me13@gmail.com>
2023-03-08 07:21:12 -0600

    update of js code and add file FishList here

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+// code provided in bk 3
+
+// Import the FishList function from the correct module
+import { getFish } from './database.js'
+
+const allFish = getFish()
+
+
+for (const fish of allFish) {
+    console.log(fish)
+}
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+const parentHTMLElement = document.querySelector("insert selector here")
+
+
+parentHTMLElement.innerHTML = FishList()
