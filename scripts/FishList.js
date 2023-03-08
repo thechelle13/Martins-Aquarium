140d6f5babff69b1e0a186c388e2ef6ef9d213d4
Michelle Totherow <tym4me13@gmail.com>
2023-03-08 10:17:07 -0600

    updates

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,160 +1,28 @@
-/*
- *  To get you started, here's some properties of Bart.
- *  You need to add more properties to complete his
- *  representation as an object. Then add all the other
- *  fish to the collection.
- */
-const database = {
-    fish: [
-        {
-            Id: 1,
-            Name: Sharkie,
-            Species: B. melanopterus,
-            Length: 2, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 2,
-            Name: Boss,
-            Species: B. splendens,
-            Length: 3, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 3,
-            Name: Bubba,
-            Species: B. melanopterus,
-            Length: 4, 
-            Origin: "Rio Negro",
-            Water: Fresh,
-            Diet: Carnivorous
-        },
-        {
-            Id: 4,
-            Name: Princess,
-            Species: P. scalare,
-            Length: 5, 
-            Origin: "Amazon Basin",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 5,
-            Name: Sara,
-            Species: B. melanopterus,
-            Length: 2, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 6,
-            Name: Gloss,
-            Species: B. splendens,
-            Length: 3, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 7,
-            Name: Bob,
-            Species: B. melanopterus,
-            Length: 4, 
-            Origin: "Rio Negro",
-            Water: Fresh,
-            Diet: Carnivorous
-        },
-        {
-            Id: 8,
-            Name: Prince,
-            Species: P. scalare,
-            Length: 5, 
-            Origin: "Amazon Basin",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 9,
-            Name: Susan,
-            Species: B. melanopterus,
-            Length: 2, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 10,
-            Name: Moss,
-            Species: B. splendens,
-            Length: 3, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 11,
-            Name: Daisy,
-            Species: B. melanopterus,
-            Length: 4, 
-            Origin: "Rio Negro",
-            Water: Fresh,
-            Diet: Carnivorous
-        },
-        {
-            Id: 12,
-            Name: King,
-            Species: P. scalare,
-            Length: 5, 
-            Origin: "Amazon Basin",
-            Water: Fresh,
-            Diet: Omnivore
-        }, 
-        {
-            Id: 13,
-            Name: Sam,
-            Species: B. melanopterus,
-            Length: 2, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 14,
-            Name: Ross,
-            Species: B. splendens,
-            Length: 3, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
-            Diet: Omnivore
-        },
-        {
-            Id: 15,
-            Name: Billy,
-            Species: B. melanopterus,
-            Length: 4, 
-            Origin: "Rio Negro",
-            Water: Fresh,
-            Diet: Carnivorous
-        },
-        {
-            Id: 16,
-            Name: Queen,
-            Species: P. scalare,
-            Length: 5, 
-            Origin: "Amazon Basin",
-            Water: Fresh,
-            Diet: Omnivore
-        }
-    ]
-}
+// Import the function that returns a copy of the fish array
+import { fish } from "./database"
 
+export const FishList = () => {
+    // Invoke the function that you imported from the database module
+    const fishes = invokeTheRightFunctionHere()
 
-// code from bk for export
-export const getFish = () => {
-    return database.fish.map(fish => ({...fish}))
+    // Start building a string filled with HTML syntax
+    const htmlString = '<article class="fishList">'
+
+    // Create HTNL representations of each fish here
+    for (const fish of fishes) {
+
+        // Why is there a backtick used for this string?
+        htmlString += `<section class="fish card">
+            <div><img  class="fish__image image--card" src="${fish.image}" /></div>
+            <div class="fish__name">${fish.name}</div>
+            <div class="fish__species">${fish.species}</div>
+            <div class="fish__length">${fish.length}</div>
+            <div class="fish__location">${fish.location}</div>
+            <div class="fish__diet">${fish.food}</div>
+        </section>
+`
+    }
+    htmlString += `</article>`
+
+    return htmlString
 }
--- scripts/database.js
+++ scripts/database.js
@@ -8,146 +8,146 @@ const database = {
     fish: [
         {
             Id: 1,
+            Image: insert,
             Name: Sharkie,
             Species: B. melanopterus,
             Length: 2, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
+            Location: "Malay Peninsula",
             Diet: Omnivore
         },
         {
             Id: 2,
+            Image: insert,
             Name: Boss,
             Species: B. splendens,
             Length: 3, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
+            Location: "Malay Peninsula",
             Diet: Omnivore
         },
         {
             Id: 3,
+            Image: insert,
             Name: Bubba,
             Species: B. melanopterus,
             Length: 4, 
-            Origin: "Rio Negro",
-            Water: Fresh,
+            Location: "Rio Negro",
             Diet: Carnivorous
         },
         {
             Id: 4,
+            Image: insert,
             Name: Princess,
             Species: P. scalare,
             Length: 5, 
-            Origin: "Amazon Basin",
-            Water: Fresh,
+            Location: "Amazon Basin",
             Diet: Omnivore
         },
         {
             Id: 5,
+            Image: insert,
             Name: Sara,
             Species: B. melanopterus,
             Length: 2, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
+            Location: "Malay Peninsula",
             Diet: Omnivore
         },
         {
             Id: 6,
+            Image: insert,
             Name: Gloss,
             Species: B. splendens,
             Length: 3, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
+            Location: "Malay Peninsula",
             Diet: Omnivore
         },
         {
             Id: 7,
+            Image: insert,
             Name: Bob,
             Species: B. melanopterus,
             Length: 4, 
-            Origin: "Rio Negro",
-            Water: Fresh,
+            Location: "Rio Negro",
             Diet: Carnivorous
         },
         {
             Id: 8,
+            Image: insert,
             Name: Prince,
             Species: P. scalare,
             Length: 5, 
-            Origin: "Amazon Basin",
-            Water: Fresh,
+            Location: "Amazon Basin",
             Diet: Omnivore
         },
         {
             Id: 9,
+            Image: insert,
             Name: Susan,
             Species: B. melanopterus,
             Length: 2, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
+            Location: "Malay Peninsula",
             Diet: Omnivore
         },
         {
             Id: 10,
+            Image: insert,
             Name: Moss,
             Species: B. splendens,
             Length: 3, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
+            Location: "Malay Peninsula",
             Diet: Omnivore
         },
         {
             Id: 11,
+            Image: insert,
             Name: Daisy,
             Species: B. melanopterus,
             Length: 4, 
-            Origin: "Rio Negro",
-            Water: Fresh,
+            Location: "Rio Negro",
             Diet: Carnivorous
         },
         {
             Id: 12,
+            Image: insert,
             Name: King,
             Species: P. scalare,
             Length: 5, 
-            Origin: "Amazon Basin",
-            Water: Fresh,
+            Location: "Amazon Basin",
             Diet: Omnivore
         }, 
         {
             Id: 13,
+            Image: insert,
             Name: Sam,
             Species: B. melanopterus,
             Length: 2, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
+            Location: "Malay Peninsula",
             Diet: Omnivore
         },
         {
             Id: 14,
+            Image: insert,
             Name: Ross,
             Species: B. splendens,
             Length: 3, 
-            Origin: "Malay Peninsula",
-            Water: Fresh,
+            Location: "Malay Peninsula",
             Diet: Omnivore
         },
         {
             Id: 15,
+            Image: insert,
             Name: Billy,
             Species: B. melanopterus,
             Length: 4, 
-            Origin: "Rio Negro",
-            Water: Fresh,
+            Location: "Rio Negro",
             Diet: Carnivorous
         },
         {
             Id: 16,
+            Image: insert,
             Name: Queen,
             Species: P. scalare,
             Length: 5, 
-            Origin: "Amazon Basin",
-            Water: Fresh,
+            Location: "Amazon Basin",
             Diet: Omnivore
         }
     ]
